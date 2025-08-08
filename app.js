5c38f1cab5cdbb0be8edc2b38f7bbee17fe7fd34
ValeriaLS <valeria.luna6387@alumnos.udg.mx>
2025-08-08 14:08:31 -0600

    Agregar parte uno

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//1.Lista para guardar los nombres
+let listaAmigos = [];
+
+//2.Funcion asignar texto a un elemento 
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
+
+//3.Funcion para agregar amigos
+function agregarAmigo() {
+    let agregarAmigo = document.getElementById('amigo').value;
+    if (agregarAmigo.trim() === '') {
+        alert('Por favor, inserte un nombre');
+        return;
+    }
+    
+    listaAmigos.push(agregarAmigo);
+    document.getElementById('amigo').value = '';
+    actualizarListaAmigos();
+}
+function actualizarListaAmigos() {
+    let listaElemento = document.getElementById('listaAmigos');
+    listaElemento.innerHTML = ''; 
+    for (const nombre of listaAmigos) {
+        let li = document.createElement('li');
+        li.textContent = nombre;
+        listaElemento.appendChild(li);
+    }
+}
+
+
+//5.Funcion para sortear amigos
+function sortearAmigo () {
+    
+}
